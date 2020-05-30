64b76b5b084d89b8a5d68d8aebe6d690f066f7a6
Luca Kanyo <lucakanyo@protonmail.com>
2020-05-30 22:43:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -49,8 +49,8 @@ class Song
   
   def self.new_from_filename(mp3)
     song = self.new 
-    title = 
-    song.name = 
+    title = mp3.split('-')[1]
+    song.name = title.slice(1, -3)
     song.artist_name = mp3.split('-')[0]
     #binding.pry
     song 
